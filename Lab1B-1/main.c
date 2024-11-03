08a89ad0724cc4a33ce7a158a93ac6a4509b744b
Sashenka Gamage <77524750+sashenkagamage@users.noreply.github.com>
2024-11-03 18:37:51 +0800

    Add files via upload

---
+++ Lab1B-1/main.c
@@ -0,0 +1,35 @@
+/*
+ * Lab1B-1.c
+ *
+ * Created: 24/9/2024 9:20:08 am
+ * Author : sashe
+ */ 
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	
+	DDRD |= (1<<2); //setting to 1 as output
+	DDRB &= ~(1 << 0); //setting to 0 as input
+	PORTD &= ~(1<<2);
+	
+	unsigned char press;
+
+
+	while (1)
+	{
+		press = PINB & (1 << 0);;
+		
+		if (press == 0){
+			PORTD &= ~(1 << 2);
+			}else{
+			PORTD |=(1<<2);
+		}
+
+	}
+	return 0;
+}
